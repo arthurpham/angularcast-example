4cf3e6708daea369f6db5275489051a0eebd513f
Arthur Pham <arthur.pham@gmail.com>
2013-05-16 08:43:00 -0400

    try to match look of coderberry.me post

--- lib/railscast_feed.rb
+++ lib/railscast_feed.rb
@@ -6,7 +6,9 @@ class RailscastFeed
     Feedzirra::Feed.add_common_feed_entry_element(:enclosure, :value => :url, :as => :video_url)
     Feedzirra::Feed.add_common_feed_entry_element('itunes:duration', :as => :duration)
 
-    feed = Feedzirra::Feed.fetch_and_parse("http://feeds.feedburner.com/railscasts")
+    # private rss http://railscasts.com/subscriptions/SHCgyzZHehc3L9XpYajM0g/episodes.rss
+    # public rss http://feeds.feedburner.com/railscasts
+    feed = Feedzirra::Feed.fetch_and_parse("http://railscasts.com/subscriptions/SHCgyzZHehc3L9XpYajM0g/episodes.rss")
     feed.entries.each do |entry|
       Episode.create_from(entry, 'railscasts')
     end
